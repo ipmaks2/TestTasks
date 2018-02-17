519940e292d25720478c1772161ad2aba1a1abcc
ipmaks2 <ipmaks2@mail.ru>
2018-02-17 20:08:51 +0300

    Prepared list of tests

--- task1.py
+++ task1.py
@@ -1,33 +1,71 @@
 '''
-Classes to find any pairs of numbers in sequence that add up to 10.
+Classes to find any pairs of numbers in sequence that add
+up to 10.
+
 Example:
 Sample input:
     1, 8, 2, 3, 5, 7
 Sample output:
-    "(8, 2), (3,7)"
+    "(2, 8), (3, 7)"
 
 Module Pairs has classes: Pair and PairFinder
 
 Assumptions:
     - numbers should be less then 10
     - zeros not acceptable
+    - let's output pair when minimal value goes first
+    - let's sort all pairs by first (minimal) number
+
 '''
-##############################################################################
+############################################################
 class Pair(object):
     '''
     Stores pair and helpers for printing them out
+
+    Methods:
+        __str__ - used for printing one pair
+        __repr__ - used for debug
+        __init__ - constructor, stores elements in ordered
+        getPair - return one pair as tuple of ordered elements
+
     '''
     def __str__(self):
         return '({}, {})'.format(self.n1, self.n2)
 
+    def __repr__(self):
+        return str(self.__dict__)
+
     def __init__(self, n1, n2):
-        self.n1 = n1
-        self.n2 = n2
+        pair = (n1, n2)
+        nMin = min(pair)
+        nMax = max(pair)
+        self.n1 = nMin
+        self.n2 = nMax
+
+    def getPair(self):
+       return (self.n1, self.n2)
 
 
 class PairFinder(object):
     '''
     Helps to find pairs of numbers.
+
+    Class attributes:
+        pairSum - stores sum of pair elements, used for
+                  finding pairs
+
+    Object attributes:
+        sequence - stores received list of elements,
+        pairs - list of all pairs found after request by
+                findPairs method
+
+
+    Methods:
+        __str__ - used for output pairs as string
+        __init__ - constructor, creates and initialize
+        assignSeq - used to pass list of elements where to search for pairs
+        findPairs - complete search of pairs in provided previously sequence
+                    returns list of objects: Pair
     '''
     pairSum = 10  # Expected sum for pair
 
@@ -40,20 +78,23 @@ class PairFinder(object):
 
     def assignSeq(self, seq):
         self.sequence = seq
+        self.pairs = []
 
     def findPairs(self):
-        seq = self.sequence
-        while (seq):
-            n1 = seq.pop() 
-            n2 = self.pairSum - n1
-            if seq.count(n2):
-                seq.pop(seq.index(n2))
-                self.pairs.append(Pair(n1, n2))
+        if not self.pairs:
+            seq = self.sequence[:]
+            while (seq):
+                n1 = seq.pop()
+                n2 = self.pairSum - n1
+                if seq.count(n2):
+                    seq.pop(seq.index(n2))
+                    self.pairs.append(Pair(n1, n2))
+            self.pairs.sort(key=lambda x: x.n1)
         return self.pairs
 
 
-##############################################################################
-##############################################################################
+############################################################
+############################################################
 
 if __name__ == '__main__':
 
@@ -63,9 +104,12 @@ if __name__ == '__main__':
     print p1
     print p2
 
+    print p1.getPair()
+    print p2.getPair()
+
     f = PairFinder()
     f.assignSeq([1,2,3,4,5,6,7,8,9] )
     f.findPairs()
     print f
-
+    print f.__dict__
 
--- testTask1.py
+++ testTask1.py
@@ -1,12 +1,98 @@
-#for Pairs:
-#1 - check pair is stored
-#2 - check pair is printed in expected form
-#3 -   
+'''
+Cmd to run tests:
+     pytest -v -s testTask1.py
 
-#for PairFinder (Assert 1 for return pairs list, assert2 - for formatted string):
+Smoke tests For PairFinder:
+
 #1 Pass Empty List
 #2 List without pairs
 #3 Odd num elements
 #4 Even num elements
-#5 List with only 1 pair 
+#5 List with only 1 pair
+
+Assert 1 for return pairs list
+assert 2 for formatted string
+Assert 3 for object count
+
+'''
+
+
+from task1 import PairFinder
+import pytest
+
+def setup_function(f):
+    '''
+    Setup for every test. Creates new object to test.
+    '''
+    f.finder = PairFinder()
+
+def teardown_function(f):
+    '''
+    Teardown for every test. Destroys object after testing.
+    '''
+    del(f.finder)
+
+def test_emptyList():
+    '''
+    Test verifies: when we pass empty list, we should
+    receive empty list of pairs
+    '''
+    f = test_emptyList.finder
+    f.assignSeq([])
+    assert [] == f.findPairs()
+    assert '' == f.__str__()
+
+
+def test_noPairs():
+    '''
+    Test verifies: when we pass list with elements,
+    but no pairs (with sum 10), we should receive
+    empty list of pairs.
+    '''
+    f = test_noPairs.finder
+    f.assignSeq([1])
+    assert [] == f.findPairs()
+    assert '' == f.__str__()
+
+
+def test_odd():
+    '''
+    Test verifies: when we pass odd number of elements,
+    we should receive all expected pairs.
+    '''
+    f = test_odd.finder
+    f.assignSeq([1, 2, 3, 5, 9, 8, 5])
+    pairs = f.findPairs()
+    assert 3 == len(pairs)
+    assert '(1, 9),(2, 8),(5, 5)' == f.__str__()
+    for pair in pairs:
+        assert pair.getPair() in [(1, 9),(5, 5),(2, 8)]
+
+def test_even():
+    '''
+    Test verifies: when we pass even number of elements,
+    we should receive all expected pairs.
+    '''
+    f = test_even.finder
+    f.assignSeq([1, 2, 3, 9, 8, 5])
+    pairs = f.findPairs()
+    assert 2 == len(pairs)
+    assert '(1, 9),(2, 8)' == f.__str__()
+    for pair in pairs:
+        assert pair.getPair() in [(1, 9),(2, 8)]
+
+def test_onePair():
+    '''
+    Test verifies: when we pass 2 elements as 1 pair,
+    we should receive this one pair.
+    '''
+    f = test_onePair.finder
+    f.assignSeq([2, 8])
+    pairs = f.findPairs()
+    assert 1 == len(pairs)
+    assert '(2, 8)' == f.__str__()
+    for pair in pairs:
+        assert pair.getPair() in [(2, 8)]
+
+
 
