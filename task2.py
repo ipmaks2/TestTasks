6017db865c0f324b07d8b09046ee289b1ef499fa
ipmaks2 <ipmaks2@mail.ru>
2018-02-18 15:47:26 +0300

    Bad solution for task2

--- task2.py
+++ task2.py
@@ -1,25 +1,44 @@
 import inspect
 import sys
 import traceback
+import gc
 
+debug = False
 
 def foo():
-    print inspect.stack()[1][4]
+    if (not  hasattr(foo, 'aliases')) :
+        foo.aliases = {'foo()': 1}
+    line =  inspect.stack()[1][4][0]
+    line = line.strip()
+    # print '"{}"'.format(line)
+    # print type(line)
+
+    if '=' in line:
+        newName, usedName = line.split('=')
+        newName = newName.strip()
+        usedName = usedName.strip()
+        # print newName
+        # print usedName
+        # print foo.aliases
+        print foo.aliases[usedName]
+        foo.aliases[newName+'()'] = foo.aliases.get(usedName) + 1
+
     return foo
 
 foo.count = 1
 
 if __name__ == '__main__':
 
-     f = foo()
-     f = f()
-     f = f()
-     f = f()
-     f = f()
+    f = foo()
+    f = f()
+    f = f()
+    f = f()
+    f = f()
 
-     b = foo()
-     c = foo()
-     b = b()
-     c = c()
-     c = c()
+    b = foo()
+    c = foo()
+    b = b()
+    c = c()
+    c = c()
+    c()
 
