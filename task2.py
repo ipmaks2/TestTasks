05cebca072e17aa6c2e575367a71fbd6f020e51f
ipmaks2 <ipmaks2@mail.ru>
2018-02-19 00:04:33 +0300

    Task2 reworked with methodcaller

--- task2.py
+++ task2.py
@@ -1,22 +1,20 @@
-import inspect
+from inspect import stack
+from operator import methodcaller
 
 def foo():
-    if (not  hasattr(foo, 'aliases')) :
+    if (not hasattr(foo, 'aliases')) :
         foo.aliases = {'foo()': 1}
     # line - we read from stack original line from src to 
     #        to parse name iused to call function 
     #        and name to assign returned function
-    line =  inspect.stack()[1][4][0]
+    line = stack()[1][4][0]
     line = line.strip()
     # works only for line like 'name = name()'
     # just to demonstrate concept, should be improved 
     if '=' in line:
-        newName, usedName = line.split('=')
-        newName = newName.strip()
-        usedName = usedName.strip()
+        newName, usedName = map(methodcaller('strip'),line.split('='))
         print foo.aliases[usedName]
         foo.aliases[newName+'()'] = foo.aliases.get(usedName) + 1
-
     return foo
 
 if __name__ == '__main__':
