05c570092ae96487892cbba172489efab05934ad
ipmaks2 <ipmaks2@mail.ru>
2018-02-19 09:29:23 +0300

    Added test orig after secondary in task2

--- task2.py
+++ task2.py
@@ -1,20 +1,31 @@
+'''
+ Module with implemented foo(), which counts
+ by what reference it was called.
+
+ It demonstartes how inspect module can be used for it.
+ Preliminary version, should be improved.
+'''
 from inspect import stack
 from operator import methodcaller
 
 def foo():
-    if (not hasattr(foo, 'aliases')) :
+    '''
+    Prints out imaginary lenght from called reference to function
+    to original name foo.
+    '''
+    if not hasattr(foo, 'aliases'):
         foo.aliases = {'foo()': 1}
-    # line - we read from stack original line from src to 
-    #        to parse name iused to call function 
+    # line - we read from stack original line from src to
+    #        to parse name iused to call function
     #        and name to assign returned function
     line = stack()[1][4][0]
     line = line.strip()
     # works only for line like 'name = name()'
-    # just to demonstrate concept, should be improved 
+    # just to demonstrate concept, should be improved
     if '=' in line:
-        newName, usedName = map(methodcaller('strip'),line.split('='))
-        print foo.aliases[usedName]
-        foo.aliases[newName+'()'] = foo.aliases.get(usedName) + 1
+        newname, usedname = map(methodcaller('strip'), line.split('='))
+        print foo.aliases[usedname]
+        foo.aliases[newname+'()'] = foo.aliases.get(usedname) + 1
     return foo
 
 if __name__ == '__main__':
--- test_task2.py
+++ test_task2.py
@@ -4,54 +4,105 @@ Cmd to run tests:
 '''
 from task2 import foo
 
-attr2del = 'aliases'
+ATTR = 'aliases'
 
 def setup_function():
-    assert hasattr(foo, attr2del) == False
+    '''
+    Prepare environment before test
+    Enshure environment is OK
+    '''
+    assert hasattr(foo, ATTR) is False
 
 def teardown_function():
-    if hasattr(foo,attr2del ):
-        delattr(foo, attr2del)
+    '''
+    Clean environment after test.
+    Clear attribute with stored call names
+    from completed test,
+    '''
+    if hasattr(foo, ATTR):
+        delattr(foo, ATTR)
 
 def test_as_expected_from_task(capsys):
+    '''
+    When called sequence as in Task #2 from mail,
+    Should be expected prints as from test task.
+    '''
     b = foo()
     c = foo()
     b = b()
     c = c()
     c = c()
-    out, err =  capsys.readouterr()
+    out, err = capsys.readouterr()
     assert out == '1\n1\n2\n2\n3\n'
+    assert err == ''
 
 def test_no_increment_with_original_name(capsys):
+    '''
+    When called with original name,
+    Should print 1
+    '''
     f = foo()
     f = foo()
     f = foo()
     f = foo()
-    out, err =  capsys.readouterr()
+    out, err = capsys.readouterr()
     assert out == '1\n1\n1\n1\n'
+    assert err == ''
+
 
 def test_no_increment_with_secondary_name(capsys):
+    '''
+    When called with secondary name several time,
+    Should print 2
+    '''
     a = foo()
     b = a()
     b = a()
     b = a()
-    out, err =  capsys.readouterr()
+    out, err = capsys.readouterr()
     assert out == '1\n2\n2\n2\n'
+    assert err == ''
+
 
 def test_increment_with_reassign_same_name(capsys):
+    '''
+    When called with reassigned same name,
+    Should print with increment.
+    '''
     f = foo()
     f = f()
     f = f()
     f = f()
-    out, err =  capsys.readouterr()
+    out, err = capsys.readouterr()
     assert out == '1\n2\n3\n4\n'
+    assert err == ''
+
 
 def test_increment_with_reassign_different_names(capsys):
+    '''
+    When called with reassigned different names,
+    Should print with increment.
+    '''
     a = foo()
     b = a()
     c = b()
     d = c()
-    out, err =  capsys.readouterr()
+    out, err = capsys.readouterr()
     assert out == '1\n2\n3\n4\n'
+    assert err == ''
 
+
+def test_original_after_secondary_name(capsys):
+    '''
+    When called with original name after reassigned,
+    Should print 1 again after increment.
+    '''
+    f = foo()
+    f = f()
+    f = f()
+    f = f()
+    f = foo()
+    out, err = capsys.readouterr()
+    assert out == '1\n2\n3\n4\n1\n'
+    assert err == ''
 
